46f1db8345f2a72b0c0eb0500c552fb931191e4a
Guillaume Maze <gmaze@ifremer.fr>
2022-05-04 09:11:55 +0200

    Update Position-soleil-static.py

--- Position-soleil-static.py
+++ Position-soleil-static.py
@@ -29,6 +29,16 @@ def daily_sun_position(day, obs):
     df['time'] = df['time'].dt.time
     return df
 
+def now_sun_position(t, obs):
+    # t = pd.to_datetime('now', utc=True)
+    # t = pd.Timestamp(t).tz_convert(tz='Europe/Paris')
+    altitude, azimuth = [], []
+    altitude.append(sun.elevation(obs, t))# The number of degrees up from the horizon at which the sun can be seen
+    azimuth.append(sun.azimuth(obs, t))   # The number of degrees clockwise from North at which the sun can be seen
+    df = pd.DataFrame(list(zip([t], azimuth, altitude)), columns=['time', 'azimuth', 'altitude'])
+    df['time'] = df['time'].dt.time
+    return df
+
 def get_xlim(this_df, vname='altitude', dx=5):
     return (
     pd.to_datetime("%s%0.2d%0.2d%0.2d"%(date,
@@ -44,7 +54,8 @@ def get_xlim(this_df, vname='altitude', dx=5):
 
 
 # Define date to work with
-t = pd.to_datetime('now', utc='Europe/Paris')
+t = pd.to_datetime('now', utc=True) # 'Europe/Paris')
+t = pd.Timestamp(t).tz_convert(tz='Europe/Paris')
 date = t.strftime("%Y%m%d")
 
 # Define reference data
@@ -66,6 +77,7 @@ solstice_winter = daily_sun_position('%s1221' % date[0:4], l.observer)
 
 # Work with specific date
 df = daily_sun_position(date, l.observer)
+df_now = now_sun_position(t, l.observer)
 
 # Plot
 # local_date = pd.to_datetime(date).strftime("%A %d %B %Y")
@@ -105,6 +117,7 @@ fig, ax = plt.subplots(ncols=2, sharey=True, figsize=(20, 7))
 
 ax[0].plot(solstice_winter['azimuth'], solstice_winter['altitude'], 'b', label="Solstice d'hiver", linewidth=2)
 ax[0].plot(df['azimuth'], df['altitude'], 'k', label=local_date, linewidth=3)
+ax[0].plot(df_now['azimuth'], df_now['altitude'], 'k.', label=df_now['time'].values[0].strftime("%H:%M"), markersize=18)
 ax[0].plot(solstice_summer['azimuth'], solstice_summer['altitude'], 'r', label="Solstice d'été", linewidth=2)
 ax[0].xaxis.set_minor_locator(MultipleLocator(10))
 ax[0].xaxis.set_major_locator(MultipleLocator(20))
@@ -121,6 +134,7 @@ ax[0].text(270-1, 90, 'W', horizontalalignment='right', verticalalignment='top',
 
 ax[1].plot(pd.to_datetime(date + " " + solstice_winter['time'].astype(str)) , solstice_winter['altitude'], 'b', label="Solstice d'hiver", linewidth=2)
 ax[1].plot(pd.to_datetime(date + " " + df['time'].astype(str)), df['altitude'], 'k', label=local_date, linewidth=3)
+ax[1].plot(pd.to_datetime(date + " " + df_now['time'].astype(str)), df_now['altitude'], 'k.', label=df_now['time'].values[0].strftime("%H:%M"), markersize=18)
 ax[1].plot(pd.to_datetime(date + " " + solstice_summer['time'].astype(str)) , solstice_summer['altitude'], 'r', label="Solstice d'été", linewidth=2)
 ax[1].xaxis.axis_date(tz='Europe/Paris')
 ax[1].xaxis.set_major_locator(dates.HourLocator())
@@ -147,11 +161,11 @@ with open('data/last_update.json', 'w') as outfile:
 with open('data/current_altitude.json', 'w') as outfile:
     json.dump({"schemaVersion": 1,
                "label": "Altitude",
-               "message": "%0.3f" % sun.elevation(l.observer, t),
+               "message": "%0.3f" % df_now['altitude'].values[0],
                "color": "green"}, outfile)
 
 with open('data/current_azimuth.json', 'w') as outfile:
     json.dump({"schemaVersion": 1,
                "label": "Azimuth",
-               "message": "%0.3f" % sun.azimuth(l.observer, t),
+               "message": "%0.3f" % df_now['azimuth'].values[0],
                "color": "green"}, outfile)
